98acd6d60d64347b0a32bfced61bb8973f170d1a
atharv656 <atharv.v.agashe.25@dartmouth.edu>
2023-04-07 15:55:29 -0400

    initial website commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="px-10 py-10 h-24" src="/NytroLogo.png"></img>
+      <h1 className="text-3xl py-20">
+        Site coming soon!
+      </h1>
     </div>
   );
 }
